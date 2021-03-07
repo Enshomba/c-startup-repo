dc690b1804b873d3f56ac631334453e95dae5433
Enshomba <78908091+Enshomba@users.noreply.github.com>
2021-03-07 00:03:29 -0800

    Add files via upload
    first commit of the start up

---
+++ start.cpp
@@ -0,0 +1,200 @@
+#include <iostream>
+
+using namespace std;
+
+// start of the main function
+
+int main(){
+	
+	// function body
+	
+	cout <<" 2+5 = " <<2+5<<endl;
+	cout <<" 13+89 = "<<13+89 <<endl;
+	cout <<" 34-20 = "<<34-20 <<endl;
+	cout <<" 45-90 = "<<45-90 <<endl;
+	cout <<" 2*7   = "<<2*7   <<endl;
+	cout <<" 5/2   = "<<5/2   <<endl;
+	cout <<" 14/7  = "<<14/7  <<endl;
+	cout <<" 34%5  = "<<34%5  <<endl;
+	cout <<" 4%6   = "<<4%6   <<endl;
+	
+	
+	
+	cout <<"here comes the testing exercise" <<endl;
+	
+	cout <<"=======================================" <<endl;
+	
+	cout <<" 5.0+3.5 = " <<5.0 + 3.5 <<endl;
+	cout <<" 3.0+9.4 = " <<3.0 + 9.4 <<endl;
+	cout <<" 16.3-5.2 = "<<16.3 - 5.2<<endl;
+	cout <<" 4.2*2.5 = " <<4.2 * 2.5 <<endl;
+	cout <<" 5.0/2.0 = " <<5.0 / 2.0 <<endl;
+	cout <<" 3.5/6.0 = " <<34.5 / 6.0<<endl;
+	cout <<" 34.5/6.5 = "<<34.5 / 6.5 <<endl;
+	
+	
+	// it really ended with the arthimetic single precision floating number
+	//from the modulo number
+	
+	cout <<"===================================================" <<endl;
+	cout << "expressions are evaluated in c++" <<endl;
+	//evaluation of mixed expressions
+	cout <<"3/2 + 5.5 = "<<3/2 + 5.5  <<endl;
+	cout <<"15.6 / 2 + 5 = " <<15.6/2+5 <<endl;
+	cout <<"4 + 5 / 2.0 =  " <<4+5/2.0 <<endl;
+	cout <<"4 * 3 + 7 /5 - 25.5 = " <<4*3+7/5 <<endl;
+	cout <<" see how explicit type works in arthimetcs" <<endl;
+	
+	cout <<"===================================================" <<endl;
+	
+	
+	cout <<" static_cast<int>(7.9) = "<<static_cast<int>(7.9) <<endl;
+	cout <<" static_cast<int>(3.3) = "<<static_cast<int>(3.3) <<endl;
+	cout <<" static_cast<double>(25)= "<<static_cast<double>(25) <<endl;
+	cout <<" static_cast<double>(5+3)= "<<static_cast<double>(5+3)<<endl;
+	cout <<" static_cast<double>(15)/2 = "<<static_cast<double>(15)/2<<endl;
+	cout <<" static_cast<double>(15/2)= "<<static_cast<double>(15/2)<<endl;
+	cout <<" static_cast<int>(7.8+ static_cast<double>(15)/2 = "<<static_cast<int>(7.8+static_cast<double>(15)/2)<<endl;
+	cout <<" static_canst<int>(7.8+static_canst<double>(15/2))= " <<static_cast<int>(7.8+static_cast<double>(15/2))<<endl;
+// those were type conversions
+//in c++
+cout <<" the value of letters cast as integers in arthmetics" <<endl;
+cout <<"======================================================"<<endl;
+
+cout <<"static_cast<int>('A') = "<< static_cast<int>('A')<<endl;
+cout <<"static_cast<int>('B') = "<< static_cast<int>('B')<<endl;
+cout <<"static_cast<int>('C') = "<< static_cast<int>('C')<<endl;
+cout <<"static_cast<int>('D') = "<< static_cast<int>('D')<<endl;
+cout <<"static_cast<int>('E') = "<< static_cast<int>('E')<<endl;
+cout <<"static_cast<int>('F') = "<< static_cast<int>('F')<<endl;
+cout <<"static_cast<int>('G') = "<< static_cast<int>('G')<<endl;
+cout <<"static_cast<int>('H') = "<< static_cast<int>('H')<<endl;
+cout <<"static_cast<int>('I') = "<< static_cast<int>('I')<<endl;
+cout <<"static_cast<int>('J') = "<< static_cast<int>('J')<<endl;
+cout <<"static_cast<int>('K') = "<< static_cast<int>('K')<<endl;
+cout <<"static_cast<int>('L') = "<< static_cast<int>('L')<<endl;
+cout <<"static_cast<int>('M') = "<< static_cast<int>('M')<<endl;
+cout <<"static_cast<int>('N') = "<< static_cast<int>('N')<<endl;
+cout <<"static_cast<int>('O') = "<< static_cast<int>('O')<<endl;
+cout <<"static_cast<int>('P') = "<< static_cast<int>('P')<<endl;
+cout <<"static_cast<int>('Q') = "<< static_cast<int>('Q')<<endl;
+cout <<"static_cast<int>('R') = "<< static_cast<int>('R')<<endl;
+cout <<"static_cast<int>('S') = "<< static_cast<int>('S')<<endl;
+cout <<"static_cast<int>('T') = "<< static_cast<int>('T')<<endl;
+cout <<"static_cast<int>('U') = "<< static_cast<int>('U')<<endl;
+cout <<"static_cast<int>('V') = "<< static_cast<int>('V')<<endl;
+cout <<"static_cast<int>('W') = "<< static_cast<int>('W')<<endl;
+cout <<"static_cast<int>('X') = "<< static_cast<int>('X')<<endl;
+cout <<"static_cast<int>('Y') = "<< static_cast<int>('Y')<<endl;
+cout <<"static_cast<int>('Z') = "<< static_cast<int>('Z')<<endl;
+//FOR THE SYMBOLS
+cout<<" this is the value of all symbols as inters cast" <<endl;
+
+cout<<"========================================================="<<endl;
+cout <<"static_cast<int>('~') = "<< static_cast<int>('~')<<endl;
+cout <<"static_cast<int>('!') = "<< static_cast<int>('!')<<endl;
+cout <<"static_cast<int>('@') = "<< static_cast<int>('@')<<endl;
+cout <<"static_cast<int>('#') = "<< static_cast<int>('#')<<endl;
+cout <<"static_cast<int>('$') = "<< static_cast<int>('$')<<endl;
+cout <<"static_cast<int>('%') = "<< static_cast<int>('%')<<endl;
+cout <<"static_cast<int>('^') = "<< static_cast<int>('^')<<endl;
+cout <<"static_cast<int>('&') = "<< static_cast<int>('&')<<endl;
+cout <<"static_cast<int>('*') = "<< static_cast<int>('*')<<endl;
+cout <<"static_cast<int>('(') = "<< static_cast<int>('(')<<endl;
+cout <<"static_cast<int>(')') = "<< static_cast<int>(')')<<endl;
+cout <<"static_cast<int>('_') = "<< static_cast<int>('_')<<endl;
+cout <<"static_cast<int>('+') = "<< static_cast<int>('+')<<endl;
+cout <<"static_cast<int>('{') = "<< static_cast<int>('{')<<endl;
+cout <<"static_cast<int>('}') = "<< static_cast<int>('}')<<endl;
+cout <<"static_cast<int>('|') = "<< static_cast<int>('|')<<endl;
+//cout <<"static_cast<int>('""') = "<< static_cast<int>('""')<<endl;
+cout <<"static_cast<int>(':') = "<< static_cast<int>(':')<<endl;
+cout <<"static_cast<int>('?') = "<< static_cast<int>('?')<<endl;
+cout <<"static_cast<int>('>') = "<< static_cast<int>('>')<<endl;
+cout <<"static_cast<int>('<') = "<< static_cast<int>('<')<<endl;
+cout <<"static_cast<int>('/') = "<< static_cast<int>('/')<<endl;
+cout <<"static_cast<int>('.') = "<< static_cast<int>('.')<<endl;
+cout <<"static_cast<int>(',') = "<< static_cast<int>(',')<<endl;
+cout <<"static_cast<int>(';') = "<< static_cast<int>(';')<<endl;
+cout <<"static_cast<int>(']') = "<< static_cast<int>(']')<<endl;
+cout <<"static_cast<int>('[') = "<< static_cast<int>('[')<<endl;
+//cout <<"static_cast<int>('\') = "<< static_cast<int>('\')<<endl;
+cout <<"static_cast<int>('-') = "<< static_cast<int>('-')<<endl;
+cout <<"static_cast<int>('=') = "<< static_cast<int>('=')<<endl;
+
+
+
+//this illustrates how data in the variables are manipulated
+
+cout <<"==========================================================="<<endl;
+int num1,num2;
+double sale;
+char first;
+string str;
+
+num1=4;
+cout <<"num1 = "<<num1 <<endl;
+
+num2 =4*5-11;
+cout <<"num2 = "<<num2 <<endl;
+
+sale = 0.02*1000;
+cout <<"sale = "<<sale <<endl;
+
+first ='D';
+cout <<"first = "<<first <<endl;
+
+str = "Its a sunny day.";
+cout <<"str = "<<str <<endl;
+
+// the following program illustrates how the input statments work
+cout <<"how th input statments wowrk in c++" <<endl;
+
+cout <<"===========================================";
+// variable declaration
+int feet;
+int inches;
+
+cout <<"Enter two integers seperated by spaces"<<endl;
+// get input from the user via keyboard
+cin >> feet >>inches;
+cout <<endl;
+
+//now outpu the variable values
+cout << "Feet = " <<feet <<endl;
+cout <<endl;
+cout <<"Inches = "<< inches<<endl;
+//
+
+
+
+//how to read string and intager variables from the keyboard
+
+//variable declaration
+string firstName;
+string lastName;
+int age;
+double weight;
+
+
+// read from the input device
+
+cout << "Enter first name, last name , age,"
+	 << "and weight."<< endl;
+	 
+	 cin >>firstName >>lastName;
+	 cin >>age >>weight;
+		 //output the read variable values from the keyboard
+		 cout <<"output the read variable values from the keyboard"<<endl;
+		 cout <<"==================================================="<<endl;
+		 
+cout <<"Name: " <<firstName <<" "
+	 << lastName <<endl;
+	 
+cout <<"Age: "<<age <<endl;
+cout <<"Weight: " <<weight <<endl;
+
+
+
+	return 0;
+}
